fb9f259c998b23cd92db9ec9ea613075df28ade5
Juuuupiter <691107824@qq.com>
2017-12-07 14:07:13 +0800

    xiami-mymusic

--- components/MyMusic/mymusic.vue
+++ components/MyMusic/mymusic.vue
@@ -7,13 +7,21 @@
       </div>
       <div class="recently">
           <div class="listen">
-
+              <img src="http://pic.xiami.net/images/collect/61/61/359923061_1512220697_KHe9.jpg@1e_1c_100Q_185w_185h">
+              <div class="listen-letter">
+                  <p>本地歌曲&nbsp&nbsp&nbsp&nbsp<i class="fa fa-play-circle-o" aria-hidden="true"></i></p>
+                  <span>0首</span>
+              </div>
           </div>
           <div class="listen">
               <img class="cdCover185" src="http://pic.xiami.net/images/album/img0/18/5907f58a2f72a_946100_1493693834.jpg@4e_1c_100Q_185w_185h" rel="v:photo" alt="志明与春娇">
-              <p>最近播放</p>
+              <div class="listen-letter">
+                  <p>最近播放&nbsp&nbsp&nbsp&nbsp<i class="fa fa-play-circle-o" aria-hidden="true"></i></p>
+                  <span>30首</span>
+              </div>
           </div>
       </div>
+      <div class="collection"></div>
       <common-footer></common-footer>
   </div>
 </template>
@@ -57,22 +65,34 @@ export default {
     .recently{
         margin-top: 0.9rem;
         width: 100%;
-        height: 2rem;
-        background: firebrick;
+        height: 2rem；
     }
     .listen{
-        /* position: absolute; */
         width: 50%;
         height: 1.6rem;
-        background: darkblue;
-        margin-top: 0.2rem;
+        margin-top: 0.1rem;
         display:inline-block;
         float: left;
+        background:whitesmoke;
     }
     .listen img{
+        float: left;
         width: 40%;
         margin-top: 0.1rem;
         -moz-box-shadow: 6px 6px 5px #888888; /* 老的 Firefox */
         box-shadow: 6px 6px 5px #888888;
+        margin-left: 0.05rem;
+    }
+    .listen-letter{
+        height: 1rem;
+        width: 55%;
+        float: right;
+        margin: 0.3rem auto;
+    }
+    .listen p{
+       font-size: 0.25rem;
+    }
+    .listen span{
+        color: darkgray
     }
 </style>
