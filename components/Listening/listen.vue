cae4408b6d02b0e35567217fd55edfb4ca078220
Juuuupiter <691107824@qq.com>
2018-04-03 22:35:38 +0800

    vue-listen

--- components/Listening/listen.vue
+++ components/Listening/listen.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="listening">
-    <a-player :music="musicList" v-if="isShow"></a-player>
+    <a-player autoplay  :music="D" v-if="isShow" :showLrc="true" :mutex="true" />
     <common-footer></common-footer>
   </div>
 </template>
@@ -13,24 +13,45 @@ export default {
        return{
          musicData:[],
          musicList:[],
+         D:{},
          isShow : false
        }
      },
      mounted(){
         Axios.get('/static/music-data.json')
         .then(res=>{
-          
           this.musicData = res.data.musicData;
           for(var i=0; i<this.musicData.length;i++){
-                  var obj   = new Object();
+                  var obj = new Object();
                   obj.title = this.musicData[i].title;
                   obj.artist= this.musicData[i].author;
                   obj.src   = this.musicData[i].src;
                   obj.pic   = this.musicData[i].musicImgSrc;
-                  obj.lrc   = this.musicData[i].lrc;
-                  this.musicList = obj;  
+                  obj.lrc   = "/static/"+this.musicData[i].lrc;
+
+                  // var music = new Object();
+                  // music[i]= obj;
+                  // var musicList = {}; 
+                  // // this.musicList.push(music[i]);
+                  
+                  // // musicList = musicList + music[i];
+                  // // musicList.filed[i]=
+                  // // console.log(music)
+                  
+                  // musicList.filed[i] = music[i]
+                  this.musicList.push(obj)
+                  var D = Object.assign(this.musicList)
+                  // var json = JSON.stringify(this.musicList);  
+                  // var jsonObj =  JSON.parse(json);
+                  // var jsonObj = String.parseJSON(json);
+                  // alert(json);  
+      
+                  // var jsonElem = document.getElementById("json");  
+                  // jsonElem.innerHTML = json; 
           }
-         
+          // this.musicObj = D
+          // console.log(D)
+          // console.log(typeof(D))
           this.isShow = true; 
         });
      },
